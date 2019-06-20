4dbf125d80700bb147fcf8c2412f990888ba3394
Aquarius <binhvm.kimson@gmail.com>
2019-06-20 08:45:56 +0700

    Update layout footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,7 +4,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p class="text-center">Cơ quan chủ quản</p>
+                    <p class="text-center">Công ty Cổ phần Quảng cáo Trực tuyến 24H Trụ sở: Tầng 03, Tòa nhà Intracom, 82 Dịch Vọng Hậu, Phường Dịch Vọng Hậu, Quận Cầu Giấy, TP Hà Nội. Tel: (84) 978 042 994.</p>
+                    <p class="text-center">Giấy phép số: chưa cấp.</p>
                 </div>
             </div>
         </div>
